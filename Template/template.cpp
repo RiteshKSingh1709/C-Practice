218ded37c29d4a703350021025f1835d98e328e7
RiteshKSingh1709 <ritesh.kumar465@gmail.com>
2019-11-03 18:26:52 +0530

    recent commit

--- Template/template.cpp
+++ Template/template.cpp
@@ -18,8 +18,22 @@ template<class T,class K> class Test{
         
 };
 
+//template method example
+// we can also use template<class T> void printIt(T t) like this ...
+template<typename T> void printIt(T t){
+    cout << t << endl;
+}
+
+//non template version to print the integer
+void print(int a){
+    cout << "Non template version : " << a << endl;
+}
+
 int main(){
     Test<string,int> test1("Hello");
     test1.print();
+    printIt<string>("Hello");
+    printIt<int>(5);
+    print(8);
     return 0;
 }
